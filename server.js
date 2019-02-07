75859adfd6a774df26d78ae26a6baec28dba02cb
Laza <postlaza@gmail.com>
2019-02-07 14:15:18 -0800

    jas

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use(routes);
 
 
 // Start the server
-aapp.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
