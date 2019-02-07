821816da0c544c8f41c8fe810ce34ed6d9735d75
Laza <postlaza@gmail.com>
2019-02-06 22:40:44 -0800

    Test5

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/nprNews", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
